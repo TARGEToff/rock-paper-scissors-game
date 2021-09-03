2b2e105be2c2ec26033928e5a72ab382f8d6e18b
Filip Glura <lockontargt@gmail.com>
2021-09-03 17:26:43 +0200

    feat: added buttons clicked action

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def clicked(type):
+    print(type)
+
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
@@ -15,12 +18,15 @@ def window():
     rockBtn = QtWidgets.QPushButton(win)
     rockBtn.setText("Rock")
     rockBtn.move(85, 80)
+    rockBtn.clicked.connect(lambda: clicked("rock"))
     paperBtn = QtWidgets.QPushButton(win)
     paperBtn.setText("Paper")
     paperBtn.move(195, 80)
+    paperBtn.clicked.connect(lambda: clicked("paper"))
     scissorsBtn = QtWidgets.QPushButton(win)
     scissorsBtn.setText("Scissors")
     scissorsBtn.move(305, 80)
+    scissorsBtn.clicked.connect(lambda: clicked("scissors"))
 
     win.show()
     sys.exit(app.exec_())
