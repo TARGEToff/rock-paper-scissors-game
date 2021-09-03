bc4151e87748850dcd4e2d29b66ea1c749d0c68f
Filip Glura <lockontargt@gmail.com>
2021-09-03 17:14:40 +0200

    feat: Added rock, paper, scissors buttons

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import sys
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
-    win.setGeometry(600, 300, 500, 400)
+    win.setGeometry(600, 300, 500, 200)
     win.setWindowTitle("Rock paper scissors")
 
     title = QtWidgets.QLabel(win)
     title.setText("Rock paper scissors")
     title.move(200, 30)
 
+    rockBtn = QtWidgets.QPushButton(win)
+    rockBtn.setText("Rock")
+    rockBtn.move(85, 80)
+    paperBtn = QtWidgets.QPushButton(win)
+    paperBtn.setText("Paper")
+    paperBtn.move(195, 80)
+    scissorsBtn = QtWidgets.QPushButton(win)
+    scissorsBtn.setText("Scissors")
+    scissorsBtn.move(305, 80)
 
     win.show()
     sys.exit(app.exec_())
